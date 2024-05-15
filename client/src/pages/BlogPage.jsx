9a3f0a14b3fe724a4d11f6a77308bc92061e93e2
injifannoo <injifanotu@gmail.com>
2024-05-15 03:30:00 +0300

    blog page is updated

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import BlogCard from '../components/BlogCard';
+import { Link } from 'react-router-dom';
 
 const LatestBlogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -17,15 +18,26 @@ const LatestBlogs = () => {
   }, []);
 
   return (
-    <div className="bg-white w-full ">
+    <div className="bg-white w-full">
       <div className="flex flex-col justify-center items-center my-10">
-        <h2 className="text-center text-3xl font-bold text-black mb-12">Blogs</h2>
+        <h2 className="text-center text-3xl font-bold  text-blue-700 mb-3">Recent Blogs</h2>
+        <hr className="w-24 h-1 bg-blue-500 border-0 rounded " />
+
         <div className="line"></div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 px-4  mx-10">
-        {blogs.map((blog) => (
-          <BlogCard key={blog._id} blog={blog} />
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 md:px-4 md:mx-10">
+        {blogs.slice(0, 3).map((blog) => (
+          <BlogCard key={blog.id} blog={blog} />
         ))}
+      </div>
+      <div className="flex justify-center mt-10">
+        <Link to="/all-blogs">
+          <button
+            className="bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-lg font-medium transition duration-200 ease-in-out px-6 py-3 rounded-full"
+          >
+            See More
+          </button>
+        </Link>
       </div>
     </div>
   );
